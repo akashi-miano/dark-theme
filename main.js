133f10b78dc290bd7b6511855b1c1438e6e59e07
Akashi <1221301995@student.mmu.edu.my>
2023-12-20 21:13:40 +0800

    Used css variables to achieve the same goal

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
 const body = document.querySelector("body");
 const toggleIcon = document.querySelector(".fa-moon");
-const btn = document.querySelector(".btn");
-const links = document.querySelectorAll(".link");
 
 toggleIcon.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  if (body.classList.contains("dark")) {
-    btn.classList.add("light");
+  body.classList.toggle("dark-theme");
+  if (body.classList.contains("dark-theme"))
     toggleIcon.classList.replace("fa-moon", "fa-sun");
-    links.forEach((link) => {
-      link.classList.add("light-links");
-    });
-  } else {
-    toggleIcon.classList.replace("fa-sun", "fa-moon");
-    btn.classList.remove("light");
-    links.forEach((link) => {
-      link.classList.remove("light-links");
-    });
-  }
+  else toggleIcon.classList.replace("fa-sun", "fa-moon");
 });
